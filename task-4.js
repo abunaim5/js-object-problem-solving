632ebced1f25279fc92787fdf0ffa01a426a19f0
abunaim5 <abunaim140@gmail.com>
2024-01-31 17:31:41 +0600

    task-4 solved

---
+++ task-4.js
@@ -0,0 +1,15 @@
+// Count the number of properties
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true,
+};
+// console.log(student);
+// const key = Object.keys(student);
+// console.log(key.length);
+const key = Object.keys(student).length;
+console.log(key);
+// for (let i = 0; i <= key.length - 1; i++){
+//     console.log(i);
+// }
